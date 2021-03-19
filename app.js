8f51807ff56d894c689c0ec9b6121d1bad3cabe9
JE <jecagurangan06@gmail.com>
2021-03-20 01:18:08 +0800

    Login Modal Effect

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ let hamburgerCollapse = true;
 const hamburger = document.querySelector(".nav__hamburger");
 const hamburgerMenu = document.querySelector(".hamburger__menu");
 
+const modal = document.querySelector(".modal");
+const login = document.querySelector(".login");
+const loginButton = document.querySelector(".login-button");
+const loginHamburger = document.querySelector(".login-hamburger");
+
 hamburger.addEventListener("click", function () {
   console.log("working");
 
@@ -20,4 +25,26 @@ hamburger.addEventListener("click", function () {
     hamburgerMenu.style.overflow = "hidden";
     hamburgerCollapse = true;
   }
+});
+
+loginButton.addEventListener("click", function () {
+  console.log("working");
+
+  modal.style.display = "block";
+  login.style.zIndex = "200";
+  login.style.opacity = "1";
+});
+
+loginHamburger.addEventListener("click", function () {
+  console.log("working");
+
+  modal.style.display = "block";
+  login.style.zIndex = "200";
+  login.style.opacity = "1";
+});
+
+modal.addEventListener("click", function () {
+  modal.style.display = "none";
+  login.style.zIndex = "0";
+  login.style.opacity = "0";
 });
