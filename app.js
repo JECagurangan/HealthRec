4b3d65189fdec085b69ce7f99fa5097220d06c9b
JE <jecagurangan06@gmail.com>
2021-03-20 12:09:29 +0800

    Sign Up Modal

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 console.log("working");
 
 let hamburgerCollapse = true;
-const hamburger = document.querySelector(".nav__hamburger");
-const hamburgerMenu = document.querySelector(".hamburger__menu");
+const hamburger = document.querySelector(".nav__hamburger"); //menu button
+const hamburgerMenu = document.querySelector(".hamburger__menu"); //menu dropdown
 
-const modal = document.querySelector(".modal");
-const login = document.querySelector(".login");
-const loginButton = document.querySelector(".login-button");
-const loginHamburger = document.querySelector(".login-hamburger");
+const modal = document.querySelector(".modal"); //dim background
+const login = document.querySelector(".login"); //login modal
+const signUp = document.querySelector(".sign-up"); //sign up modal
+const loginButton = document.querySelector(".login-button"); //login from desktop
+const loginHamburger = document.querySelector(".login-hamburger"); //login from mobile
+
+const join = document.querySelector("#join"); //join healthrec from signin
+const backToLogin = document.querySelector("#back-to-login"); //back to login from register
 
 hamburger.addEventListener("click", function () {
   console.log("working");
@@ -45,6 +49,22 @@ loginHamburger.addEventListener("click", function () {
 
 modal.addEventListener("click", function () {
   modal.style.display = "none";
-  login.style.zIndex = "0";
+  login.style.zIndex = "-5";
   login.style.opacity = "0";
+  signUp.style.zIndex = "-5";
+  signUp.style.opacity = "0";
+});
+
+join.addEventListener("click", function () {
+  login.style.zIndex = "-5";
+  login.style.opacity = "0";
+  signUp.style.zIndex = "200";
+  signUp.style.opacity = "1";
+});
+
+backToLogin.addEventListener("click", function () {
+  signUp.style.zIndex = "-5";
+  signUp.style.opacity = "0";
+  login.style.zIndex = "200";
+  login.style.opacity = "1";
 });
